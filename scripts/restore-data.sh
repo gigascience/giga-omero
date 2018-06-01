1ef4a78ba53a7d87a15b65c39c64c302c6842f09
pli888 <peter@gigasciencejournal.com>
2018-06-01 17:26:36 +0800

    Update AWS provision scripts
    Moved dependency install and nginx conf configuration into
    separate scripts.

--- scripts/restore-data.sh
+++ scripts/restore-data.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-sudo wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo rpm -ivh epel-release-latest-7.noarch.rpm
-sudo yum install -y librsync rdiff-backup
-
 sudo rdiff-backup --restore-as-of now --force /vagrant/backup/data /OMERO
